f43e02503d901903c06302042608df28cc533cae
Melany555 <mvelascon@estudiante.uniajc.edu.co>
2025-02-11 16:37:27 +0000

    creacion de op

--- Clase2/ejercicio1.java
+++ Clase2/ejercicio1.java
@@ -2,9 +2,9 @@ public class ejercicio1{
     public static void main  (String [] args){
         int [] a = {5, 8, 4, 10, 2, 18};
 
-        for (int i = 0; i<a.length; i++){
-            System.out.println("a[ " + i + " ]= "+ a[i]);
-        }
+        OperacionesConArreglos op = new OperacionesConArreglos();
+
+        System.out.println(op.mostrarElementos(a));
 
     }
 
